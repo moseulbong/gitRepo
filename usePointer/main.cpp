5cc00efb7dde5d89eda3a081d246b0432492bef4
moseulbong <moseulbong@gmail.com>
2019-08-24 07:22:39 +0900

    usePointer 1..

---
+++ usePointer/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int x;
+	int* p_int;
+
+	p_int = &x;
+	cout << "Enter a number : ";
+	cin >> x;
+	cout << "number is " << *p_int << '\n';
+
+	return 0;
+}
